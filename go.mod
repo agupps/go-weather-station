7fd907abb2b993d559ff7fb8ff5cedc340754e60
Aditya Gupta <aditya.gupta@appian.com>
2023-11-21 11:26:54 -0500

    Creation of a client and a test file for the weather class

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.21.3
 
 require (
 	fyne.io/fyne/v2 v2.4.1
-	github.com/briandowns/openweathermap v0.19.0
 )
 
 require (
---
+++ internal/weather/weather_test.go
@@ -0,0 +1,90 @@
+package weather_test
+
+import (
+	"io"
+	"log/slog"
+	"net/http"
+	"os"
+	"strings"
+	"testing"
+	"weather-station/internal/weather"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type HTTPMock struct {
+}
+
+func (client HTTPMock) Get(zipCode string) (*http.Response, error) {
+	stringReader := strings.NewReader(sampleResponse)
+	
+	exampleResponse := &http.Response{
+		Body: io.NopCloser(stringReader),
+		Status: http.StatusText(200),
+	}
+	return exampleResponse, nil
+}
+
+var sampleResponse = `
+{
+	"coord": {
+	  "lon": 10.99,
+	  "lat": 44.34
+	},
+	"weather": [
+	  {
+		"id": 501,
+		"main": "Rain",
+		"description": "moderate rain",
+		"icon": "10d"
+	  }
+	],
+	"base": "stations",
+	"main": {
+	  "temp": 298.48,
+	  "feels_like": 298.74,
+	  "temp_min": 297.56,
+	  "temp_max": 300.05,
+	  "pressure": 1015,
+	  "humidity": 64,
+	  "sea_level": 1015,
+	  "grnd_level": 933
+	},
+	"visibility": 10000,
+	"wind": {
+	  "speed": 0.62,
+	  "deg": 349,
+	  "gust": 1.18
+	},
+	"rain": {
+	  "1h": 3.16
+	},
+	"clouds": {
+	  "all": 100
+	},
+	"dt": 1661870592,
+	"sys": {
+	  "type": 2,
+	  "id": 2075663,
+	  "country": "IT",
+	  "sunrise": 1661834187,
+	  "sunset": 1661882248
+	},
+	"timezone": 7200,
+	"id": 3163858,
+	"name": "Zocca",
+	"cod": 200
+  }                        
+`
+
+func TestCurrentWeather_Call(t *testing.T){
+	
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+
+	mockClient := HTTPMock{}
+
+	w := weather.New(mockClient, "21163", logger)
+	w.Call()
+	assert.Equal(t, w.Main.Temp, 298.48)
+}
